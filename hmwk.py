64e654ce1246e1eaf828f38d3777a1eae3dc0db4
alenthomas <alenthomas9@gmail.com>
2015-03-30 17:25:09 +0530

    adds hmwk solutions

--- hmwk.py
+++ hmwk.py
@@ -1,16 +1,89 @@
+from collections import OrderedDict
 def palindrome(var):
     length = len(var)
     tmp = ""
-#    tmp = var
     for i in range(length-1, -1, -1):
-     #       print "%s" % var[i]
             tmp += var[i]
                 
-    #print var
-    #print tmp
     if (tmp == var):
-       # print "True"
+        print "True"
         return True
     else:
-        #print"False"
+        print "False"
         return False
+##def palindrome(var):
+#     return var==var[::-1]
+def tables(num):
+    list_tables = []
+    for i in range(0, 10):
+        list_tables.append((i+1)*num)
+        print "{:2} x {:2} = {:2}".format(i+1, num, list_tables[i])
+    return list_tables
+
+def fizzbizz():
+    fizz = []
+    for i in range (1, 101):
+        if ((i % 3) == 0 and (i % 5) == 0): #if(i%15)==0
+            print "fizzbizz"
+            fizz.append("fizzbizz")
+        elif i % 5 == 0:
+            print "bizz"
+            fizz.append("bizz")
+        elif i % 3 == 0:
+            print "fizz"
+            fizz.append("fizz")
+        else:
+            print i
+            fizz.append(i)
+    return fizz
+
+def panagram(stng):
+    count = 0
+    chara = "abcdefghijklmnopqrstuvwxyz"
+    stng = stng.lower()
+    for i in chara:
+        if i in stng:
+            count = count + 1
+    if count == 26:
+        return True
+    else:
+        return False
+
+def prime(num):
+    limit = []
+    prime = []
+    composite = []
+    for i in range (2, num+1):
+        limit.append(i)
+
+    for j in limit:
+        if j not in composite:
+            for k in range(j*2, num+1, j):
+                composite.append(k)
+
+    for i in limit:
+        if i not in composite:
+            prime.append(i)
+    return prime
+
+def frequency(char):
+    dict_freq = {}
+    dict_ordered = {}
+    frq_list = []
+    for i in char:
+        if i not in dict_freq:
+            dict_freq.update({i:1})
+        else:
+            dict_freq[i]+=1
+    for k in dict_freq:
+           print "%s :: %s" % ( k, dict_freq[k])
+
+    return dict_freq
+    
+
+tables(4)
+tables(9)
+palindrome("alen")
+palindrome("malayalam")
+fizzbizz()
+frequency("she sells sea shells on the sea shore")
--- test_hmwk.py
+++ test_hmwk.py
@@ -5,3 +5,38 @@ def test_palindrome():
     assert palindrome("angel") == False
     assert palindrome("madam") == True
 test_palindrome()
+
+
+def test_tables():
+    no9 = []
+    no4 = []
+    for i in range(1, 11):
+         no9.append(9*i)
+         no4.append(4*i)
+         
+    assert tables(9) == no9
+    assert tables(4) == no4
+
+def test_fizzbizz():
+    list_values = [1, 2, 'fizz', 4, 'bizz', 'fizz', 7, 8, 'fizz', 'bizz', 11, 'fizz', 13, 14, 'fizzbizz', 16, 17, 'fizz', 19, 'bizz', 'fizz', 22, 23, 'fizz', 'bizz', 26, 'fizz', 28, 29, 'fizzbizz', 31, 32, 'fizz', 34, 'bizz', 'fizz', 37, 38, 'fizz', 'bizz', 41, 'fizz', 43, 44, 'fizzbizz', 46, 47, 'fizz', 49, 'bizz', 'fizz', 52, 53, 'fizz', 'bizz', 56, 'fizz', 58, 59, 'fizzbizz', 61, 62, 'fizz', 64, 'bizz', 'fizz', 67, 68, 'fizz', 'bizz', 71, 'fizz', 73, 74, 'fizzbizz', 76, 77, 'fizz', 79, 'bizz', 'fizz', 82, 83, 'fizz', 'bizz', 86, 'fizz', 88, 89, 'fizzbizz', 91, 92, 'fizz', 94, 'bizz', 'fizz', 97, 98, 'fizz', 'bizz']
+    assert fizzbizz() == list_values
+
+def test_panagram():
+    all_letters = "sphinx of black quartz, judge my vow"
+    rand = "alen"
+    assert panagram(all_letters) == True
+    assert panagram(rand) == False
+
+def test_prime():
+    till50 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+    till10 = [2, 3, 5, 7]
+    assert prime(50) == till50
+    assert prime(10) == till10
+
+def test_frequency():
+    dict_sea = { ' ': 7,'a': 2, 's': 8, 'r': 1, 'e': 7, 't': 1, 'h': 4, 'l': 4, 'o': 2, 'n': 1}
+    sea = "she sells sea shells on the sea shore"
+    greeting = "helloalenhowareyou"
+    dict_greeting = {'h':2,'e':3,'l':3,'o':3,'a':2,'n':1,'w':1,'r':1,'y':1,'u':1}
+    assert frequency(sea) == dict_sea
+    assert frequency(greeting) == dict_greeting
