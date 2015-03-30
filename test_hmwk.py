7daa47acd29f7f3706f5454c32c95332e833e1b6
Alen Thomas <alenthomas9@gmail.com>
2015-03-30 17:30:55 +0530

    formats the python code

--- test_hmwk.py
+++ test_hmwk.py
@@ -18,7 +18,14 @@ def test_tables():
     assert tables(4) == no4
 
 def test_fizzbizz():
-    list_values = [1, 2, 'fizz', 4, 'bizz', 'fizz', 7, 8, 'fizz', 'bizz', 11, 'fizz', 13, 14, 'fizzbizz', 16, 17, 'fizz', 19, 'bizz', 'fizz', 22, 23, 'fizz', 'bizz', 26, 'fizz', 28, 29, 'fizzbizz', 31, 32, 'fizz', 34, 'bizz', 'fizz', 37, 38, 'fizz', 'bizz', 41, 'fizz', 43, 44, 'fizzbizz', 46, 47, 'fizz', 49, 'bizz', 'fizz', 52, 53, 'fizz', 'bizz', 56, 'fizz', 58, 59, 'fizzbizz', 61, 62, 'fizz', 64, 'bizz', 'fizz', 67, 68, 'fizz', 'bizz', 71, 'fizz', 73, 74, 'fizzbizz', 76, 77, 'fizz', 79, 'bizz', 'fizz', 82, 83, 'fizz', 'bizz', 86, 'fizz', 88, 89, 'fizzbizz', 91, 92, 'fizz', 94, 'bizz', 'fizz', 97, 98, 'fizz', 'bizz']
+    list_values = [1, 2, 'fizz', 4, 'bizz', 'fizz', 7, 8, 'fizz', 'bizz', 11, 'fizz', 13, 
+    14, 'fizzbizz', 16, 17, 'fizz', 19, 'bizz', 'fizz', 22, 23, 'fizz', 'bizz', 26, 'fizz', 
+    28, 29, 'fizzbizz', 31, 32, 'fizz', 34, 'bizz', 'fizz', 37, 38, 'fizz', 'bizz', 41, 'fizz', 43, 
+    44, 'fizzbizz', 46, 47, 'fizz', 49, 'bizz', 'fizz', 52, 53, 'fizz', 'bizz', 56, 'fizz', 58, 
+    59, 'fizzbizz', 61, 62, 'fizz', 64, 'bizz', 'fizz', 67, 68, 'fizz', 'bizz', 71, 'fizz', 73, 
+    74, 'fizzbizz', 76, 77, 'fizz', 79, 'bizz', 'fizz', 82, 83, 'fizz', 'bizz', 86, 'fizz', 88, 
+    89, 'fizzbizz', 91, 92, 'fizz', 94, 'bizz', 'fizz', 97, 98, 'fizz', 'bizz']
+    
     assert fizzbizz() == list_values
 
 def test_panagram():
@@ -30,6 +37,7 @@ def test_panagram():
 def test_prime():
     till50 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
     till10 = [2, 3, 5, 7]
+    
     assert prime(50) == till50
     assert prime(10) == till10
 
